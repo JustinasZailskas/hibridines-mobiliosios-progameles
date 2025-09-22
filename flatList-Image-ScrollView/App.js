3a71e05850a5b0b8e2aece5d9b75c8ad91eb956f
JustinasZailskas <justinaszailskas@gmail.com>
2025-09-22 10:34:55 +0300

    Stiliu pavyzdys

--- flatList-Image-ScrollView/App.js
+++ flatList-Image-ScrollView/App.js
@@ -1,20 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { marginLeft: 40 }]}>
+      <Text style={[styles.message, styles.warning]}>Some Text</Text>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 20,
+    padding: 10,
+    backgroundColor: '#f0f0f0',
+  },
+  message: {
+    fontSize: 18,
+    color: '#333',
+  },
+  warning: {
+    color: 'red',
+    fontWeight: 'bold',
   },
 });
